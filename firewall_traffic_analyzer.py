59eddd182837ce3b7ec2f6fa42100c0e4396074b
Kevin Hormaza <khormaza@fingerlakes.edu>
2025-10-06 01:50:17 +0000

    firewall_traffic

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -1,8 +1,9 @@
 # FILE NAME - firewall_traffic_analyzer.py
-
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Kevin Hormaza
+# DATE: October 5, 2025
+# BRIEF DESCRIPTION:  This program analyzes network traffic data and categorizes risk based on 
+# the port number and data transfer size. It outputs a summary of the 
+# network activity and an appropriate risk assessment message
 
 
 
@@ -15,13 +16,29 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+print("=== Network Traffic Security Analyzer ===")
 
 
+port_number = int(input("Enter the port number (e.g., 80, 22, 443, 3389): "))
+transfer_size = int(input("Enter the data transfer size in megabytes (MB): "))
 
 
+if port_number == 22 and transfer_size > 500:
+    risk_assessment = "HIGH RISK: Potential unauthorized remote access detected!"
+elif port_number == 3389 and transfer_size >= 100:
+    risk_assessment = "HIGH RISK: Potential remote desktop data exfiltration detected!"
+elif port_number == 80 and transfer_size > 100:
+    risk_assessment = "MEDIUM RISK: Large unencrypted data transfer detected."
+elif port_number == 443:
+    risk_assessment = "LOW RISK: Secure encrypted transfer detected."
+else:
+    risk_assessment = "UNKNOWN: Unrecognized traffic pattern."
 
 
-
+print("\nFIREWALL LOG:")
+print(f"Port: {port_number}, Transfer Size: {transfer_size} MB")
+print(f"Risk Assessment: {risk_assessment}")
+print("------------------------")
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
@@ -89,7 +106,10 @@ Risk Assessment: UNKNOWN: Unrecognized traffic pattern.
 '''
 
 1. Did you get tripped up using the `or` or `and` operators? If so, how?
-
+I didn’t have major issues, but I had to think carefully about
+how the and operator works in compound conditions.
+It helped to test my code and see that all the conditions
+needed to be true for the statement to run.
 
 
 
